e2439e1d94f5d4794411fdc56542d0bef87aad81
danielpodgornyy <107817241+danielpodgornyy@users.noreply.github.com>
2023-05-26 14:00:27 -0500

    Commented code & fixed bug where lightening and darkening could not be erased

--- README.md
+++ README.md
@@ -4,16 +4,18 @@
 
 What I learned:
 
-* How to use a grid instead of a flexbox
-* More indepth knowledge into eventListeners
+* How to use a grid instead of a flex-box
+* More in-depth knowledge into eventListeners
 * Changing HTML & CSS using JavaScript
 * How to layout JavaScript
 
 Bugs-to-Fix / Features to Add:
 
-* Lightening and darkening can only be changed back using lightening & darkening
+* ~~Lightening and darkening can only be changed back using lightening & darkening~~
 * Zooming out messes with the page layout
 * If the grid is still on while scaling the button will appear on
 * Add a dropper to grab a color from the board
 
 [Live Preview](https://danielpodgornyy.github.io/sketch/)
+
+###### * Refactor code in 6 months *
--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//NOTE: You can refactor the code by instead of adding and removing eventListeners, use global booleans (6 months from now)
+
+//Check if mouse is down
 let mouseDown;
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+//generate page
 BuildBoard();
 Draw();
+
+/*    Page is generated    */
 
 function BuildBoard()
 {
@@ -13,24 +19,29 @@ function BuildBoard()
     board.classList.add('board');
     body.insertBefore(board, title);
 
+    //default dimensions are 32 x 32 = 1024
     for (let i = 0; i < 1024; i++)
     {
         let tile = document.createElement('div');
         tile.classList.add('tile');
+
+        //default brightness is set to 1
         tile.style.filter = 'brightness(1)';
+
         board.appendChild(tile);
     }
 }
 
 function Draw()
 {
-    ChangeScale();
+    //all options are called before you can draw
     ChangeGrid();
-    Erase();
-    Clear();
     Rainbow();
     Lighten();
     Darken();
+    Erase();
+    Clear();
+    ChangeScale();
 
     let tiles = document.querySelectorAll('.tile');
     
@@ -42,16 +53,6 @@ function Draw()
         tile.addEventListener('mouseover', Paint);
         tile.addEventListener('mousedown',Paint);
     });
-}
-
-function Paint(e)
-{
-    if (e.type === 'mouseover' && !mouseDown) return;
-
-    let colorSelector = document.querySelector('#color-picker');
-    let selectedColor = colorSelector.value;
-
-    e.currentTarget.style.backgroundColor = selectedColor;
 }
 
 function ChangeGrid()
@@ -70,6 +71,7 @@ function ChangeGrid()
             });
             borderOn = true;
 
+            //changes button to show when it is on
             e.currentTarget.style.backgroundColor = 'black';
             e.currentTarget.style.color = 'white';
         }
@@ -89,53 +91,6 @@ function ChangeGrid()
 
 }
 
-function Erase()
-{
-    let eraseButton = document.querySelector('#eraser')
-    let colorSelector = document.querySelector('#color-picker');
-    
-    eraseButton.addEventListener('click', (e) =>
-    {
-        let button = e.currentTarget;
-
-        colorSelector.value = '#FFFFFF';
-
-        button.style.backgroundColor = 'black';
-        button.style.color = 'white';
-
-        setTimeout(() =>
-        {
-            button.style.backgroundColor = 'white';
-            button.style.color = 'black';
-        },100)
-    });
-}
-
-function Clear()
-{
-    let clearButton = document.querySelector('#clear');
-    let tiles = document.querySelectorAll('.tile');
-
-    clearButton.addEventListener('click', (e) =>
-    {
-        tiles.forEach((tile) => 
-        {
-            tile.style.backgroundColor = 'white';
-        });
-
-        let button = e.currentTarget;
-        
-        button.style.backgroundColor = 'black';
-        button.style.color = 'white';
-
-        setTimeout(() =>
-        {
-            button.style.backgroundColor = 'white';
-            button.style.color = 'black';
-        },100)
-    });
-}
-
 function Rainbow()
 {
     let tiles = document.querySelectorAll('.tile');
@@ -145,8 +100,8 @@ function Rainbow()
     rainbowButton.addEventListener('click', (e) =>
     {
         if (rainbowOn == false)
-        {
-
+        {  
+            //turning off Paint is not necessary because PaintRainbow this overrides it
             tiles.forEach((tile) => {
                 tile.addEventListener('dragstart', (e) => {
                     e.preventDefault();
@@ -199,51 +154,6 @@ function GetRandColor()
     }
 
     return color;
-}
-
-function ChangeScale()
-{
-    let slider = document.querySelector('#slider');
-    let sliderDesc = document.querySelector('#slider-desc');
-
-    slider.min = '1';
-
-    slider.onmousemove =  () =>
-    {
-        sliderDesc.textContent = `${slider.value} x ${slider.value}`;
-    }
-
-    slider.onchange = () =>
-    {
-        ReloadSite(slider.value);
-    }
-}
-
-function ReloadSite(inputDimensions)
-{
-    let body = document.querySelector('body');
-    let title = document.querySelector('.title');
-    let board = document.querySelector('.board');
-
-
-    body.removeChild(board);
-
-    let newBoard = document.createElement('div');
-    newBoard.classList.add('board');
-    newBoard.style.gridTemplateColumns = `repeat(${inputDimensions}, 1fr)`;
-    newBoard.style.gridTemplateRows = `repeat(${inputDimensions}, 1fr)`;
-
-    body.insertBefore(newBoard, title);
-
-
-    for (let i = 0; i < (inputDimensions * inputDimensions); i++)
-    {
-        let tile = document.createElement('div');
-        tile.classList.add('tile');
-        newBoard.appendChild(tile);
-    }
-
-    Draw();
 }
 
 function Lighten()
@@ -376,4 +286,155 @@ function PaintDark(e)
 
     let currentBrightness = +e.currentTarget.style.filter.slice(11,-1);
     e.currentTarget.style.filter = `brightness(${currentBrightness - .10})`;
+}
+
+function Erase()
+{
+    let tiles = document.querySelectorAll('.tile');
+    let eraseButton = document.querySelector('#eraser')
+    let eraseOn = false;
+    
+    eraseButton.addEventListener('click', (e) =>
+    {
+        if (eraseOn == false)
+        {
+            tiles.forEach((tile) => {
+                tile.addEventListener('dragstart', (e) => {
+                    e.preventDefault();
+                });
+                tile.addEventListener('mouseover', PaintErase);
+                tile.addEventListener('mousedown',PaintErase);
+            });
+
+            //turn off the Paint function while erase is on
+            tiles.forEach((tile) => {
+
+                tile.removeEventListener('dragstart', (e) => {
+                    e.preventDefault();
+                });
+                tile.removeEventListener('mouseover', Paint);
+                tile.removeEventListener('mousedown',Paint);
+            });
+
+            eraseOn = true;
+
+            e.currentTarget.style.backgroundColor = 'black';
+            e.currentTarget.style.color = 'white';
+        }else
+        {
+            tiles.forEach((tile) => {
+                tile.removeEventListener('dragstart', (e) => {
+                    e.preventDefault();
+                });
+                tile.removeEventListener('mouseover', PaintErase);
+                tile.removeEventListener('mousedown',PaintErase);
+
+            });
+
+            //turn the Paint function back on
+            tiles.forEach((tile) => {
+
+                tile.addEventListener('dragstart', (e) => {
+                    e.preventDefault();
+                });
+                tile.addEventListener('mouseover', Paint);
+                tile.addEventListener('mousedown',Paint);
+            });
+
+            eraseOn = false;
+
+            e.currentTarget.style.backgroundColor = 'white';
+            e.currentTarget.style.color = 'black';
+        }
+    });
+}
+
+function PaintErase(e)
+{
+    if (e.type === 'mouseover' && !mouseDown) return;
+
+    e.currentTarget.style.backgroundColor = 'white';
+    e.currentTarget.style.filter = 'brightness(1)';
+}
+
+function Clear()
+{
+    let clearButton = document.querySelector('#clear');
+    let tiles = document.querySelectorAll('.tile');
+
+    clearButton.addEventListener('click', (e) =>
+    {
+        tiles.forEach((tile) => 
+        {
+            tile.style.backgroundColor = 'white';
+            tile.style.filter = 'brightness(1)';
+        });
+
+        //Clear does not stay on, so a slight indicator for it being on it made
+        let button = e.currentTarget;
+        
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
+
+        setTimeout(() =>
+        {
+            button.style.backgroundColor = 'white';
+            button.style.color = 'black';
+        },100)
+    });
+}
+
+function ChangeScale()
+{
+    let slider = document.querySelector('#slider');
+    let sliderDesc = document.querySelector('#slider-desc');
+
+    slider.min = '1';
+
+    slider.onmousemove =  () =>
+    {
+        sliderDesc.textContent = `${slider.value} x ${slider.value}`;
+    }
+
+    slider.onchange = () =>
+    {
+        ReloadSite(slider.value);
+    }
+}
+
+function ReloadSite(inputDimensions)
+{
+    let body = document.querySelector('body');
+    let title = document.querySelector('.title');
+    let board = document.querySelector('.board');
+
+    body.removeChild(board);
+
+    let newBoard = document.createElement('div');
+    newBoard.classList.add('board');
+    newBoard.style.gridTemplateColumns = `repeat(${inputDimensions}, 1fr)`;
+    newBoard.style.gridTemplateRows = `repeat(${inputDimensions}, 1fr)`;
+
+    body.insertBefore(newBoard, title);
+
+
+    for (let i = 0; i < (inputDimensions * inputDimensions); i++)
+    {
+        let tile = document.createElement('div');
+        tile.classList.add('tile');
+        newBoard.appendChild(tile);
+    }
+    
+    //after page is reloaded draw is called in order for its query selector to reselect the new grid
+    Draw();
+}
+
+function Paint(e)
+{
+    if (e.type === 'mouseover' && !mouseDown) return;
+
+    let colorSelector = document.querySelector('#color-picker');
+    let selectedColor = colorSelector.value;
+
+    e.currentTarget.style.backgroundColor = selectedColor;
 }
